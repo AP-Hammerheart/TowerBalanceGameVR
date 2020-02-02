6c1531f976b644370ee0cb5ef9c2fb74f2f3c744
Modersohn16 <ines.rakoczy@gmail.com>
2020-02-02 14:51:57 +0100

    Game over text

---
+++ Assets/GameOverTxt/timerGameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timerGameOver : MonoBehaviour
+{
+    public GameObject controller;  // object to which levelController script is attached to
+    float timer;
+    public Text txt; // text object for timer
+    bool isDead = true;
+    bool done = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead && !done)
+        {
+            timer = controller.GetComponent<LevelController>().Timer;
+            txt.text += timer.ToString();
+            done = true;
+        }
+    }
+}
